a56b870f2cf6174474090d78656f43684edaba5d
parkheeyeun <bom0704@naver.com>
2023-09-16 22:24:53 +0900

    counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  int count = 0;
+
+  void onclick() {
+    setState(() {
+      count = count + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MyApp();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count!',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                '$count',
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              IconButton(
+                onPressed: onclick,
+                icon: const Icon(
+                  Icons.add_box_rounded,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
